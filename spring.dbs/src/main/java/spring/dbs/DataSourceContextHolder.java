349e3c572d1217997234e32ac68b5f37aabf24a9
frinder.liu <frinder.liu@vipshop.com>
2016-12-21 18:34:56 +0800

    .

--- spring.dbs/src/main/java/spring/dbs/DataSourceContextHolder.java
+++ spring.dbs/src/main/java/spring/dbs/DataSourceContextHolder.java
@@ -7,14 +7,27 @@ public class DataSourceContextHolder {
 
     private static final ThreadLocal<String> DATASOURCE_KEY = new ThreadLocal<>();
 
+    /**
+     * 设置 datasourceKey
+     *
+     * @param datasourceKey
+     */
     public static void setDatasourceKey(String datasourceKey) {
         DATASOURCE_KEY.set(datasourceKey);
     }
 
+    /**
+     * 获取 datasourceKey
+     *
+     * @return
+     */
     public static String getDatasourceKey() {
         return DATASOURCE_KEY.get();
     }
 
+    /**
+     * 删除 datasourceKey
+     */
     public static void clearDatasourceKey() {
         DATASOURCE_KEY.remove();
     }
